9709f68325c3966a36c7b3b4c4a3ea78b0313561
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-06-06 18:02:25 +0530

    Implement main application structure with routing and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import Navbar from "./components/Navbar";
+import Mainroutes from "./routes/Mainroutes";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="py-10 px-[10%] w-screen h-screen text-white font-thin bg-gray-800">
+      <Navbar />
+      <Mainroutes />
+    </div>
+  );
 };
 
 export default App;
