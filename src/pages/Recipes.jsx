89041543d74dabd43e85dcf6bc8de6f94c090817
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-06-07 02:50:23 +0530

    Implement Recipes page to utilize RecipeContext and render recipe data

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,5 +1,16 @@
+import { useContext } from "react";
+import { recipecontext } from "../context/RecipeContext";
+
 const Recipes = () => {
-  return <div>Recipes</div>;
+  const { data } = useContext(recipecontext);
+
+  const renderrecipes = data.map((recipe) => (
+    <div key={recipe.id}>
+      <h1>{recipe.title}</h1>
+      <h1>{recipe.chef}</h1>
+    </div>
+  ));
+  return <div>{renderrecipes}</div>;
 };
 
 export default Recipes;
