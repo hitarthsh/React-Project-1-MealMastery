67db8d4ae77b3d166010c0d07ae49864b5c73aad
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-06-06 17:04:23 +0530

    Add ToastContainer for toast notifications in main component

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // ✅ Import added
 import "./index.css";
 import App from "./App.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <ToastContainer />
   </BrowserRouter>
 );
