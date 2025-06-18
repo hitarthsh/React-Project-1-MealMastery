f78b98d12b656bd85bc90576431433b432e884b6
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-06-18 13:36:51 +0530

    Refactor RecipeContext to remove commented-out recipes and disable console logging; implement axios instance for API calls and add product fetching functionality in Home component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import axios from "../utils/axios";
+// import axios from "axios";
 
 const Home = () => {
-  return <div>Home</div>;
+  const getproduct = async () => {
+    try {
+      const response = await axios.get("/products");
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <button
+        className="bg-amber-300 rounded-3xl text-1xl mt-3 px-2 py-2"
+        onClick={getproduct}
+      >
+        Get Products
+      </button>
+    </div>
+  );
 };
 
 export default Home;
